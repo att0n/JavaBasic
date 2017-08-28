ad54c3270a99c75276bb52b209aa5a337bc35e16
attoooon <c0114016dd@edu.teu.ac.jp>
2017-08-28 17:40:14 +0900

    PTra06_01からPTra06_06まで完了

--- Practice/src/practice06/PTra06_06.java
+++ Practice/src/practice06/PTra06_06.java
@@ -19,6 +19,16 @@ public class PTra06_06 {
 		 * ■■■□□
 		 * ■■■■□
 		 */
+		for(int i=0; i<5; i++) {
+			for(int j=0; j<5; j++) {
+				if(i<=j) {
+					System.out.print("□");
+				}else {
+					System.out.print("■");
+				}
+			}
+			System.out.println("");
+		}
 
 	}
 }
