deacc1264e33cb3516609efc18e56f5aa13483ab
attoooon <c0114016dd@edu.teu.ac.jp>
2017-08-28 18:33:46 +0900

    PTra07_01からPTra07_06まで完了

--- Practice/src/practice07/PTra07_06.java
+++ Practice/src/practice07/PTra07_06.java
@@ -10,12 +10,16 @@ public class PTra07_06 {
 	public static void main(String[] args) {
 
 		// ★ PTra07_05.javaで作成したランダム値が入った配列arrayを作成してください
-
+		int[] array = {41888,24254,65223,25985,68000};
 
 		System.out.println("配列の中身を小さい順に並び替えます");
 
 		// ★ 配列arrayの中身を、昇順(数値が小→大になるように)に並び替えてください
 		// 並び替えにはArrays.sort(array);を使用してくださいb。
+		java.util.Arrays.sort(array);
+		for(int i=0; i<array.length; i++) {
+			System.out.println(array[i]);
+		}
 
 	}
 }
