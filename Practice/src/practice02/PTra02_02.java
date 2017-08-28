2b862f3148bcd3c11396b98216e376819466d4da
attoooon <c0114016dd@edu.teu.ac.jp>
2017-08-28 14:53:21 +0900

    PTra02_01からPTra02_10まで完了

--- Practice/src/practice02/PTra02_02.java
+++ Practice/src/practice02/PTra02_02.java
@@ -12,6 +12,7 @@ public class PTra02_02 {
 		System.out.println(num);
 
 		// ★ 変数numの値に30足した数を出力してください
+		num += 30;
 
 
 		// ★ 以下のプログラムで40が出力されるようにしてください
--- Practice/src/practice02/PTra02_10.java
+++ Practice/src/practice02/PTra02_10.java
@@ -21,6 +21,10 @@ public class PTra02_10 {
 		 * ※ 日本円には小数点がありませんので、整数の値で出力してください(少数部分は切り捨て)
 		 * ※ プログラムは何行書いても良いです
 		 */
+		double coffee_s = coffee + coffee*0.08;
+		double snack_s = snack + snack*0.08;
+		double money = moneyInHand - (coffee_s*2 + snack) ;
+		System.out.println((int)money);
 
 	}
 }
