1b9e85aa2e6367c60bfbd108c01b8b4b9321b84a
attoooon <c0114016dd@edu.teu.ac.jp>
2017-08-30 13:43:24 +0900

    PTra10_01から06 まで完了

--- Practice/src/practice10/PTra10_05.java
+++ Practice/src/practice10/PTra10_05.java
@@ -27,16 +27,16 @@ public class PTra10_05 {
 
 		// Carクラスを作成後に着手してください
 		// ★ Car型の変数carを宣言し、Carクラスのインスタンスを代入してください
-
+		Car car = new Car();
 
 		// ★ 変数carに格納されているインスタンスのserialNoフィールドに、10000を代入してください
-
+		car.serialNo = 10000;
 
 		// ★ 変数carに格納されているインスタンスのcolorフィールドに、"Red"を代入してください
-
+		car.color = "Red";
 
 		// ★ 変数carに格納されているインスタンスのgasolineフィールドに、50を代入してください
-
+		car.gasoline = 50;
 
 		// 目的地までの距離
 		final int distance = 300;
@@ -47,6 +47,21 @@ public class PTra10_05 {
 		 * ★ 目的地についた時点で「目的地にまでn時間かかりました。残りのガソリンは、xリットルです」を出力してください
 		 * ※n：runメソッドを実行した回数, xは残りのガソリンの数です
 		 */
+		int n = 0;
+		int x;
+		int d = distance;
+
+		while(d>0) {
+			d -= car.run();
+			n++;
+			x = car.gasoline;
+			if(car.run()==-1) {
+				System.out.println("目的地に到達出来ませんでした");
+				break;
+			}else if(d<=0) {
+				System.out.println("目的地まで" + n + "時間かかりました。残りのガソリンは" + x + "リットルです");
+			}
+		}
 
 	}
 }
--- Practice/src/practice10/PTra10_06.java
+++ Practice/src/practice10/PTra10_06.java
@@ -1,4 +1,5 @@
 package practice10;
+import java.util.Arrays;
 /*
  * PTra10_06.java
  *   作成	LIKEIT	2017
@@ -18,6 +19,36 @@ public class PTra10_06 {
 		 * ★ carインスタンスを3件作成し、それぞれの色、ガソリンを入力して決定してください
 		 * ★ 各carインスタンスのrunメソッドを実行して、200km先の目的地に到達した順位を出力してください
 		 */
+		Car car1 = new Car();
+		Car car2 = new Car();
+		Car car3 = new Car();
 
+		car1.serialNo = 10000;
+		car1.color = "Red";
+		car1.gasoline = 200;
+		car2.serialNo = 20000;
+		car2.color = "Blue";
+		car2.gasoline = 200;
+		car3.serialNo = 30000;
+		car3.color = "Green";
+		car3.gasoline = 200;
+
+		int distance = 200;
+		int distance1 = 0;
+		int distance2 = 0;
+		int distance3 = 0;
+		String line = "";
+
+		while(distance1<=200 && distance2<=200 && distance3<=200) {
+			distance1 += car1.run();
+			distance2 += car2.run();
+			distance3 += car3.run();
+			line = distance1 + "," + distance2 + "," + distance3;
+		}
+		String[] ranking = line.split(",",0);
+		Arrays.sort(ranking);
+		for(int i=2; i>=0; i--) {
+			System.out.println(ranking[i]);
+		}
 	}
 }
