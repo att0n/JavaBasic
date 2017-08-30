506c8bd6301dc7766489db008371b3f82862025d
attoooon <c0114016dd@edu.teu.ac.jp>
2017-08-30 15:11:23 +0900

    PTra12_01完了

--- Practice/src/practice12/common/Student.java
+++ Practice/src/practice12/common/Student.java
@@ -29,4 +29,49 @@ public class Student {
 
 	/** パスワード */
 	public String password;
+
+
+
+	/**ID*/
+	public int getStudentId() {
+		return this.studentId;
+	}
+	public void setStudentId(int id) {
+		this.studentId = id;
+	}
+	/**氏名*/
+	public String getStudentName() {
+		return this.studentName;
+	}
+	public void setStudentName(String name) {
+		this.studentName = name;
+	}
+	/**会社名*/
+	public String getCompanyName() {
+		return this.companyName;
+	}
+	public void setCompanyName(String name) {
+		this.companyName = name;
+	}
+	/**教室名*/
+	public String getClassName() {
+		return this.className;
+	}
+	public void setClassName(String name) {
+		this.className = name;
+	}
+	/**メールアドレス*/
+	public String getMail() {
+		return this.mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	/**パスワード*/
+	public String getPassword() {
+		return this.password;
+	}
+	public void setPassword(String pass) {
+		this.password = pass;
+	}
 }
